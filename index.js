ed08d4aa3abd74277b562849e98ee03dc42d7ac7
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-10 21:06:55 -0500

    early signup page,form use, to be refactored later

--- index.js
+++ index.js
@@ -2,7 +2,32 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello it is working');
+  res.send(`
+  <div>
+    <form method="POST">
+        <input name="email" placeholder="email">
+        <input name="password" placeholder="password">
+        <input name="passwordConfirmation" placeholder="passwordConfirmation">
+        <button>Sign up</button>
+    </form>
+  </div>
+  `);
+});
+
+// app.get('/signup', (req, res) => {
+//   res.render(signup.html);
+// });
+app.post('/', (req, res) => {
+  req.on('data', data => {
+    const parsed = data.toString('utf8').split('&');
+    const formData = {};
+
+    for (let pair of parsed) {
+      const [key, value] = pair.split('=');
+      formData[key] = value;
+    }
+    console.log(formData);
+  });
 });
 
 app.listen(3000, () => {
