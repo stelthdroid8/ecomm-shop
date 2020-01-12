c8a5e26a8c953ed5bd31c05695662defe92958c5
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-12 13:44:56 -0500

    refactor for template HTML

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,11 +1,16 @@
+const layout = require('../layout');
+
 module.exports = ({ req }) => {
-  return ` <div>
-  Your ID is : ${req.session.userId}
+  return layout({
+    content: `
+    <div>
+    Your ID is : ${req.session.userId}
     <form method="POST">
         <input name="email" placeholder="email">
         <input name="password" placeholder="password">
         <input name="passwordConfirmation" placeholder="passwordConfirmation">
         <button>Sign up</button>
     </form>
-  </div>`;
+  </div>`
+  });
 };
