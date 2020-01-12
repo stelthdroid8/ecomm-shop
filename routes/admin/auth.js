117fc743bac3a8334ece1a0e9e2116d1d513447e
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-12 15:02:52 -0500

    make use of express-validator and refactor to new file for readability and reusability

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,31 +1,35 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
+
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
-
+const {
+  requireEmail,
+  requirePassword,
+  requirePasswordConfirmation
+} = require('./validators');
 const router = express.Router();
 
 router.get('/signup', (req, res) => {
   res.send(signupTemplate({ req }));
 });
 
-router.post('/signup', async (req, res) => {
-  // info from form
-  const { email, password, passwordConfirmation } = req.body;
-
-  const existingUser = await usersRepo.getOneBy({ email });
-  if (existingUser) {
-    return res.send('email in use');
+router.post(
+  '/signup',
+  [requireEmail, requirePassword, requirePasswordConfirmation],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    // info from form
+    const { email, password, passwordConfirmation } = req.body;
+    // create the user in the repo
+    const user = await usersRepo.create({ email, password });
+    //store ID inside of user cookie
+    req.session.userId = user.id;
+    res.send('account was created!');
   }
-  if (password !== passwordConfirmation) {
-    return res.send('passwords do not match');
-  }
-  // create the user in the repo
-  const user = await usersRepo.create({ email, password });
-  //store ID inside of user cookie
-  req.session.userId = user.id;
-  res.send('account was created!');
-});
+);
 
 router.get('/signout', (req, res) => {
   req.session = null;
