30f6fec15b1fb6308f8c055553d789cc8753b2ca
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-13 12:01:11 -0500

    fix passwordConfirmation validator to be async

--- routes/admin/validators.js
+++ routes/admin/validators.js
@@ -21,11 +21,9 @@ module.exports = {
     .trim()
     .isLength({ min: 4, max: 20 })
     .withMessage('Must be between 4 and 20 characters')
-    .custom((passwordConfirmation, { req }) => {
+    .custom(async (passwordConfirmation, { req }) => {
       if (passwordConfirmation !== req.body.password) {
         throw new Error('Passwords must match');
-      } else {
-        return true;
       }
     }),
   requireEmailExists: check('email')
