8afe914926f3ad28a43659297bf62e60543d0d89
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-13 14:53:31 -0500

    change encoding on new form to multipart/form-data

--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -3,7 +3,7 @@ const { getErrors } = require('../../helpers');
 module.exports = ({ errors }) => {
   return layout({
     content: `
-    <form method="POST">
+    <form method="POST" enctype="multipart/form-data">
         <input placeholder="Title" name = "title"/>
         <input placeholder="Price" name = "price"/>
         <input type="file" name = "image"/>
