1ea16238059bdf5f64751de75b8c35736cbaface
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-13 16:09:22 -0500

    add error handling and display into form

--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -1,11 +1,13 @@
 const layout = require('../layout');
-const { getErrors } = require('../../helpers');
+const { getError } = require('../../helpers');
 module.exports = ({ errors }) => {
   return layout({
     content: `
     <form method="POST" enctype="multipart/form-data">
         <input placeholder="Title" name = "title"/>
+        ${getError(errors, 'title')}
         <input placeholder="Price" name = "price"/>
+        ${getError(errors, 'price')}
         <input type="file" name = "image"/>
         <button>Submit</button>
     </form>
