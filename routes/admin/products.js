0b07c6406993bd8ab681e324e8e914d88963cec1
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-13 12:17:35 -0500

    initial structure for admin product panel & skeleton routes

---
+++ routes/admin/products.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = exprss.Router();
+
+// GET /products
+
+// GET /product/new
+//form to upload new products
+
+// POST /product/new
+//submit new products to DB
+
+// GET /products/productID
+//bring up editable form
+
+//  PUT /products/productID
+//submit the edit
+
+// DELETE /products/productID
+module.exports = router;
