415af54ef95bcc5ba5cc26ff77f51a1ac2fe0a86
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-15 13:08:49 -0500

    add product to cart functionality & keep track of cart ID via cookie

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,32 @@
 const express = require('express');
-
+const cartsRepo = require('../repositories/carts');
 const router = express.Router();
+
+router.post('/cart/products/', async (req, res) => {
+  //   console.log(req.body.productId);
+  //find cart or make new cart ID
+  let cart;
+  if (!req.session.cartId) {
+    //need to make new cart and store on cookie
+    cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cart.id;
+  } else {
+    cart = await cartsRepo.getOne(req.session.cartId);
+  }
+  // add new product or increment quantity
+  const existingItem = cart.items.find(item => item.id === req.body.productId);
+  if (existingItem) {
+    //increment quantity and save to cart
+    existingItem.quantity++;
+  } else {
+    //add new product into cart
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+  await cartsRepo.update(cart.id, {
+    items: cart.items
+  });
+  //   console.log(cart);
+  res.send('product added');
+});
 
 module.exports = router;
